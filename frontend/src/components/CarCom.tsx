508a0dcdf5af568b2b4cb9298ebc5e110889b1a5
khangerel14 <khangerelbeochir@gmail.com>
2024-09-04 11:57:26 +0800

    delete wheel

--- frontend/src/components/CarCom.tsx
+++ frontend/src/components/CarCom.tsx
@@ -7,8 +7,8 @@ export const CarCom = () => {
         <div className="relative">
           <Car />
         </div>
-        <div className="absolute z-30 h-[110px] w-[110px] right-[335px] top-[155px]"><BackWheel /></div>
-        <div className="absolute z-30 h-[110px] w-[110px] left-[350px] top-[155px]"><FrontWheel /></div>
+        {/* <div className="absolute z-30 h-[110px] w-[110px] right-[335px] top-[155px]"><BackWheel /></div> */}
+        {/* <div className="absolute z-30 h-[110px] w-[110px] left-[350px] top-[155px]"><FrontWheel /></div> */}
       </div>
       <div className="border-slate-900 border top-52 relative"></div>
     </div>
